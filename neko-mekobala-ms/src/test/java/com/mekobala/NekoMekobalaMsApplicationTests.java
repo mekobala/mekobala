124b4f90df796f54e90436e145e3e1b9d5f8a050
mekobala <842494112@qq.com>
2022-08-23 10:55:29 +0800

    [update]:空框架提交

---
+++ neko-mekobala-ms/src/test/java/com/mekobala/NekoMekobalaMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mekobala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekoMekobalaMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
